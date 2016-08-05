748fc6ff30b5f674afb360b4fcb8cf66fa5ad09c
Ethan Koenig <ethantkoenig@gmail.com>
2016-08-04 21:58:27 -0700

    Don't support python 3.2

--- setup.py
+++ setup.py
@@ -21,9 +21,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.7",
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
     ],
     keywords=["gogs", "http", "client"],
     packages=find_packages(),
